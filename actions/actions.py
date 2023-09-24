a56d5d4d62fc2c940ed1b1794b5923ca41fdb365
ramamimu <ahhmaad.blogger@gmail.com>
2023-09-24 19:52:13 +0700

    feat: file list

--- actions/actions.py
+++ actions/actions.py
@@ -7,23 +7,30 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
+from typing import Any, Coroutine, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.interfaces import Tracker
+from rasa_sdk.types import DomainDict
 
-from actions.action_modules.item import config
+from actions.resources import file_resources, IndexTopic
 
-class ActionHelloWorld(Action):
 
+class ActionFileList(Action):
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_daftar_file"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict
+    ) -> List[Dict[Text, Any]]:
+        _text = ""
+        prefix = f"kami mempunyai daftar file sebanyak {len(file_resources)}\n"
+        names = file_resources["names"]
+        # urls = file_resources["urls"]
+        for idx, name in enumerate(names):
+            temp_text = f"{idx+1} - {name}\n"
+            _text += temp_text
 
-        dispatcher.utter_message(text=config)
-
+        dispatcher.utter_message(text=_text)
         return []
---
+++ actions/resources/__init__.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+file_resources = {
+    "names": [
+        "qna akademik kemahasiswaan IF",
+        "kalender akademik ITS 2023 - 2024",
+        "daftar mata kuliah IF",
+        "Daftar Dosen IF",
+        "Peraturan Akademik",
+    ],
+    "urls": [
+        "https://itsacid-my.sharepoint.com/:x:/r/personal/ary_shiddiqi_if_its_ac_id/_layouts/15/Doc.aspx?sourcedoc=%7BF44B561E-C98D-449E-8809-D08862AD4D10%7D&file=Q%20dan%20A%20Akademik%20dan%20Kemahasiswaan%20-%20S1.xlsx&action=default&mobileredirect=true",
+        "https://itsacid-my.sharepoint.com/:x:/r/personal/ary_shiddiqi_if_its_ac_id/_layouts/15/Doc.aspx?sourcedoc=%7BCC688BB5-9033-445B-A03D-D6218028550C%7D&file=Kalender%20Akademik%20ITS%202023-2024%20ver%20%201.xlsx&action=default&mobileredirect=true",
+        "https://itsacid-my.sharepoint.com/:x:/r/personal/ary_shiddiqi_if_its_ac_id/_layouts/15/Doc.aspx?sourcedoc=%7B62B52BA4-007D-49E0-AD10-E1805382A5D3%7D&file=Daftar%20MK%20IF%20-%20wajib%20dan%20pilihan.xlsx&action=default&mobileredirect=true",
+        "https://itsacid-my.sharepoint.com/:x:/r/personal/ary_shiddiqi_if_its_ac_id/_layouts/15/Doc.aspx?sourcedoc=%7B62B52BA4-007D-49E0-AD10-E1805382A5D3%7D&file=Daftar%20MK%20IF%20-%20wajib%20dan%20pilihan.xlsx&action=default&mobileredirect=true",
+        "https://itsacid-my.sharepoint.com/personal/ary_shiddiqi_if_its_ac_id/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Fary%5Fshiddiqi%5Fif%5Fits%5Fac%5Fid%2FDocuments%2FITS%2FMANAGEMENT%2FPERKULIAHAN%2FRESOURCE%20AKADEMIK%20DAN%20KEMAHASISWAAN%2FPeraturan%20Akademik%202018%2Epdf&parent=%2Fpersonal%2Fary%5Fshiddiqi%5Fif%5Fits%5Fac%5Fid%2FDocuments%2FITS%2FMANAGEMENT%2FPERKULIAHAN%2FRESOURCE%20AKADEMIK%20DAN%20KEMAHASISWAAN",
+    ],
+}
+
+
+class IndexTopic(Enum):
+    QNA_AKADEMIK: 0
+    KALENDER_AKADEMIK: 1
+    DAFTAR_MATA_KULIAH: 2
+    DAFTAR_DOSEN: 3
+    PERATURAN_AKADEMIK: 4
